d266ceb8669e15577f7da1a8a9a8c1ad0533ca62
Jabid Abdul Hamid <jabid@kudosfinance.in>
2022-01-05 17:09:00 +0530

    sick

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -5,10 +5,10 @@ const { studentData, addStudent } = require("../controllers/studentControllers")
 const { signup, login } = require("../controllers/loginControllers")
 const auth = require("../middlewares/auth")
 
-router.get("/student/data", auth, studentData)
-router.post("/student/save", auth, addStudent)
 router.post("/student/signup", signup)
 router.post("/student/login", login)
+router.get("/student/data", auth, studentData)
+router.post("/student/save", auth, addStudent)
 router.get("/student/testauth", auth, studentData);
 
 module.exports = router;
